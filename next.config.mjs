4ada03f0c0d9db8cae84ab068cce2ee307415994
TsubasaShimizu <s432283@gmail.com>
2025-06-30 23:43:18 +0900

    no message

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const repoName = '/VESTA-LANDING-PAGE';
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -9,6 +11,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
+  ...(process.env.NODE_ENV === 'production' && {
+    basePath: repoName,
+    assetPrefix: repoName + '/',
+  }),
 }
 
 export default nextConfig

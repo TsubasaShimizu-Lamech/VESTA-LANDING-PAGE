57c30e15f0a6ad128d24c89fa992d98dccf3346e
TsubasaShimizu <s432283@gmail.com>
2025-07-04 14:46:14 +0900

    本番デザイン崩れ解消検証1

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-const repoName = '/VESTA-LANDING-PAGE';
 
 const nextConfig = {
   eslint: {
@@ -12,11 +11,6 @@ const nextConfig = {
     unoptimized: true,
   },
   output: 'export',
-  // カスタムドメインを使用する場合はbasePathとassetPrefixを無効化
-  ...(process.env.NODE_ENV === 'production' && !process.env.CUSTOM_DOMAIN && {
-    basePath: repoName,
-    assetPrefix: repoName + '/',
-  }),
-}
+};
 
-export default nextConfig
+export default nextConfig;
